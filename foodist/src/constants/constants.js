fb939a31bab40f22140db480fbc6a753ee8217aa
Sanjay krishnan <er.sanjaykrishnan@gmail.com>
2023-12-26 08:17:14 +0530

    Update constants.js

--- foodist/src/constants/constants.js
+++ foodist/src/constants/constants.js
@@ -12,7 +12,9 @@ export const swiggy_api_URL =
 
 // Swiggy API to get Restaurant Menu data with corsproxy
 export const swiggy_menu_api_URL =
-  "https://crossproxy.org/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
+  "https://github.com/ShaikImamPasha/cross/blob/main/app1.mjs"
+// export const swiggy_menu_api_URL =
+//   "https://crossproxy.org/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
 
 
 
