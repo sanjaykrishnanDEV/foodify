6d5f0227f65ab81417ed94df77bcdb0ebe16dc01
Sanjay krishnan <er.sanjaykrishnan@gmail.com>
2024-06-27 13:51:29 +0530

    Update vite.config.js

--- foodist/vite.config.js
+++ foodist/vite.config.js
@@ -1,12 +1,21 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "test-fiw",
+    project: "javascript-react"
+  })],
+
   server: {
     proxy: {
       "/api": "localhost:8080",
     },
   },
+
+  build: {
+    sourcemap: true
+  }
 });
